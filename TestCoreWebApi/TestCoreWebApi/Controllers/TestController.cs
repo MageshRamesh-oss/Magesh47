bc3f2cdccd8551ac477bb8a2b1584e1308472a01
MageshRamesh-oss <93188923+MageshRamesh-oss@users.noreply.github.com>
2021-11-25 16:51:58 +0530

    Add files via upload

---
+++ TestCoreWebApi/TestCoreWebApi/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCoreWebApi.Models;
+
+namespace TestCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMethod()
+        {
+            return Ok("Sample Output");
+        }
+        [HttpPost]
+        public IActionResult PostMethod(Property property)
+        {
+            property.Status = "SUS0011";
+            property.Message = "Successfully Fetched";
+            property.Data = property.Data;
+            return Ok(property);
+        }
+
+    }
+}
---
+++ TestCoreWebApi/TestCoreWebApi/Models/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCoreWebApi.Models
+{
+    public class Property
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public List<SecondClass> Data { get; set; }
+
+    }
+    public class SecondClass
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
