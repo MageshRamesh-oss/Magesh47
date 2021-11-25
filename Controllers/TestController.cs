ff5ae7428e0c98dffacbf667ba8208de8d9e4ca3
MageshRamesh-oss <93188923+MageshRamesh-oss@users.noreply.github.com>
2021-11-25 16:58:09 +0530

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication35.Models;
+
+namespace WebApplication35.Controllers
+{
+   
+    [Route("api/test")]
+    public class TestController : ApiController
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetElement()
+        {
+            return Ok("Simple Content");
+        }
+        [HttpPost]
+        public IHttpActionResult PostElement([FromBody]DummyClass dummyClass)
+        {
+            dummyClass.Status = "SUC0077";
+            dummyClass.Message = "Successfully Fetched";
+            dummyClass.data = dummyClass.data;
+
+            return Ok(dummyClass);
+        }
+    }
+}
---
+++ Models/DummyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication35.Models
+{
+    public class DummyClass
+    {      
+        public string Status { get; set; }
+        public string Message { get; set; }
+        
+        public List<IdName> data { get; set; }
+    }
+    public class IdName
+    {
+        public string Name;
+
+        public string Address;
+    }
+}
